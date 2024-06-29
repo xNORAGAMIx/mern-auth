4697f28dfd03daaa835a30e4f771169f85175e7b
Manas Das <noragami0021@gmail.com>
2024-06-30 01:16:44 +0530

    signin route and jwt authentication

--- BACKEND/api/controllers/authController.js
+++ BACKEND/api/controllers/authController.js
@@ -1,6 +1,7 @@
 import { User } from "../model/userModel.js";
 import asyncHandler from "express-async-handler";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const signUp = asyncHandler(async (req, res) => {
   /** Destructuring the request body */
@@ -28,7 +29,7 @@ export const signUp = asyncHandler(async (req, res) => {
 
   /**hashing user password */
   const hashPassword = await bcrypt.hash(password, 10);
-  console.log("Hashed password: ",hashPassword);
+  console.log("Hashed password: ", hashPassword);
 
   /**Creating a document in collection users in DB */
   const user = await User.create({
@@ -39,5 +40,36 @@ export const signUp = asyncHandler(async (req, res) => {
 
   res.status(201).json({
     data: user,
+  });
+});
+
+export const signIn = asyncHandler(async (req, res) => {
+  /**Fetch the email and password from form */
+  const { email, password } = req.body;
+  /** Check if user exits in database*/
+  const validUser = await User.findOne({ email });
+
+  /**User not found */
+  if (!validUser) {
+    res.status(404);
+    throw new Error("User not found!");
+  }
+
+  /**Validate password*/
+  const validPassword = await bcrypt.compare(password, validUser.password);
+
+  /**Wrong credentials */
+  if (!validPassword) {
+    res.status(401);
+    throw new Error("Invalid Credentials!");
+  }
+
+  /**Create a token */
+  const token = jwt.sign({ id: validUser._id }, process.env.ACCESS_TOKEN);
+  /**destructure user and remove password from result */
+  const { password: hashPassword, ...rest } = validUser._doc;
+  /**Add token to cookie */
+  res.cookie("access_token", token, { httpOnly: true }).status(200).json({
+    data: rest,
   });
 });
