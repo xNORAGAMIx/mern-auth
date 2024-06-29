a68a37c828929390ba958967800cd66b600e720e
Manas Das <noragami0021@gmail.com>
2024-06-30 00:42:29 +0530

    signup backend and frontend connection and functionality, added proxy to vite config for CORS

--- FRONTEND/src/pages/SignUp.jsx
+++ FRONTEND/src/pages/SignUp.jsx
@@ -1,30 +1,62 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setError(null);
+    const response = await fetch("/api/auth/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await response.json();
+    setLoading(false);
+    console.log(data);
+    if (data.title) {
+      setError(data.title);
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
           placeholder="Username"
           id="username"
+          onChange={handleChange}
           className="bg-slate-200 p-3 rounded-lg"
         />
         <input
           type="email"
           placeholder="Email"
           id="email"
+          onChange={handleChange}
           className="bg-slate-200 p-3 rounded-lg"
         />
         <input
           type="password"
           placeholder="Password"
           id="password"
+          onChange={handleChange}
           className="bg-slate-200 p-3 rounded-lg"
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          Sign Up
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign Up"}
         </button>
       </form>
       <div className="flex gap-3 mt-5">
@@ -33,6 +65,7 @@ const SignUp = () => {
           <span className="text-blue-600">Sign In</span>
         </Link>
       </div>
+      <p className="text-red-700">{error && "Something went wrong!"}</p>
     </div>
   );
 };
