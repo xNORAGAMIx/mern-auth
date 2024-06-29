71029576bc5b9c0bf019468bdb8d49176f801511
Manas Das <noragami0021@gmail.com>
2024-06-29 18:31:06 +0530

    connection to mongoDB

--- BACKEND/api/app.js
+++ BACKEND/api/app.js
@@ -1,9 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "../config/dbConnection.js";
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
-
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`Server started at ${5000}`);
+  console.log(`Server started at ${port}`);
 });
