cb62ac023936df5308aaa9919eb52b6658454220
Manas Das <noragami0021@gmail.com>
2024-07-01 08:50:14 +0530

    Home and About template

--- FRONTEND/src/pages/About.jsx
+++ FRONTEND/src/pages/About.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+
 
 const About = () => {
   return (
     <div>
-      About
+      <div className="flex flex-col gap-4 px-4 py-12 max-w-2xl mx-auto">
+        <h1 className="text-3xl font-semibold mb-4 text-slate-800">
+          About
+        </h1>
+        <p className="text-slate-700 mb-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+          aut veritatis vel. Mollitia voluptate ipsum ipsam deleniti in? Ab
+          consectetur, commodi incidunt eveniet quae rerum perferendis libero
+          tenetur recusandae! Ex.
+        </p>
+        <p className="text-slate-700 mb-4">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis
+          ducimus molestias corrupti? Eveniet itaque modi asperiores tenetur
+          ullam nihil eaque eum cupiditate mollitia et dignissimos beatae totam,
+          doloribus animi explicabo.
+        </p>
+        <p className="text-slate-700 mb-4">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate
+          vero aspernatur non. Dicta fugiat eaque porro! Nesciunt quam, officiis
+          magni nobis ullam dolore veritatis unde saepe praesentium animi
+          numquam enim!
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
