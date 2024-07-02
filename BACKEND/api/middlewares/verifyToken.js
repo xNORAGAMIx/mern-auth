c2a18ff807a10d442c85d089b55226a4dcb5fc4a
Manas Das <noragami0021@gmail.com>
2024-07-02 23:52:48 +0530

    debugging token

--- BACKEND/api/middlewares/verifyToken.js
+++ BACKEND/api/middlewares/verifyToken.js
@@ -3,6 +3,8 @@ import jwt from "jsonwebtoken";
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
 
+  console.log(token);
+  
   if (!token) {
     res.status(401);
     throw new Error("You need to login!");
