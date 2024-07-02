ed27c39a08bdbd6cc7a4f3feba744d6417612b9d
Manas Das <noragami0021@gmail.com>
2024-07-02 23:58:29 +0530

    frontend url

--- BACKEND/api/index.js
+++ BACKEND/api/index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["https://mern-auth-frontend-five.vercel.app"],
+    origin: ["https://mern-auth-frontend-rho.vercel.app"],
     methods: ["POST", "GET", "DELETE"],
     credentials: true,
   })
